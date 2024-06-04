6edc69f50b2517ce498a34837d1b623357a4f518
samira <samirabarreto2020@gmail.com>
2024-06-03 22:23:36 -0300

    tipos e variaveis

---
+++ estudos/java_basico/tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double VALOR_DE_PI = 3.14;
+
+        // VALOR_DE_PI = 4.5;
+    }
+}
